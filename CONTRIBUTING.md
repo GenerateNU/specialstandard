d64f3a08dc4c28c95260175d6faa4bcd8e3706c8
adescoteaux1 <ally.descoteaux.0@gmail.com>
2025-08-29 18:22:41 -0400

    backend + db init + docs

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,1 +1,19 @@
 # Contributing
+
+## Overview
+
+To submit changes, create a pull request and fill out the provided template. **Make sure that the pull request title is descriptive.**
+
+## Backend PRs
+
+If you're writing backend code, please include a screenshot of your endpoint(s) working (Postman, Insomnia, etc.). Please also include the URL of your endpoint and any parameters you passed in - this is super helpful for other developers looking to use your endpoint.
+
+## Frontend PRs
+
+If you're writing frontend code, please include a screenshot or screen recording of your new/updated pages.
+
+## Review process
+
+Once submitted, your pull request will require at least one of the tech leads' approval. Additionally, your code will be automatically linted and built using GitHub Actions. This is super important to keep our main branch clean and well-tested. Once the CI checks pass and an approval is given, you're free to merge your PR!
+
+If your pull request is reviewed and not approved, the reviewer will leave some suggestions for changes. (Sometimes we will approve and still leave suggestions). Once you have made your changes, make sure to re-request a review and/or message one of the TLs on Slack to ensure another review.
---
+++ backend/cmd/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"os"
+	"os/signal"
+	"specialstandard/internal/config"
+	"specialstandard/internal/service"
+	"syscall"
+
+	"github.com/joho/godotenv"
+	"github.com/sethvargo/go-envconfig"
+)
+
+func main() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+		return
+	}
+
+	var config config.Config
+	if err := envconfig.Process(context.Background(), &config); err != nil {
+		log.Fatalln("Error processing .env file: ", err)
+	}
+
+	app := service.InitApp(config)
+
+	// Pushing the closing of the database connection onto a
+	// stack of statements to be executed when this function returns.
+
+	// **Uncomment after repo connection is actually made**
+	// defer app.Repo.Close()
+
+	port := config.Application.Port
+	// Listen for connections with a goroutine.
+	go func() {
+		if err := app.Server.Listen(":" + port); err != nil {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// Wait for the termination signal:
+	<-quit
+
+	// Then shutdown server gracefully.
+	slog.Info("Shutting down server")
+	if err := app.Server.Shutdown(); err != nil {
+		slog.Error("failed to shutdown server", "error", err)
+	}
+
+	slog.Info("Server shutdown")
+}
