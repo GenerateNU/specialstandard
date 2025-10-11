0b1de99f347c18a76c999f7055788fd8a461688a
Harsh31105 <harshnirajsingh@gmail.com>
2025-10-11 19:29:55 -0400

    feat: Added AWS S3 as a Client and downloaded dependencies.

---
+++ backend/internal/s3/s3Client.go
@@ -0,0 +1,28 @@
+package s3
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type Client struct {
+	S3     *s3.Client
+	Bucket string
+}
+
+func New(bucketName string) (*Client, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return nil, fmt.Errorf("unable to load AWS SDK config: %w", err)
+	}
+
+	s3Client := s3.NewFromConfig(cfg)
+
+	return &Client{
+		S3:     s3Client,
+		Bucket: bucketName,
+	}, nil
+}
