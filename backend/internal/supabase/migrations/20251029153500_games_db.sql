9f2e509790caba40089171075d6bdadfb07cfbfd
Harsh31105 <harshnirajsingh@gmail.com>
2025-11-06 02:31:18 -0500

    feat: Migration Script.

--- backend/internal/supabase/migrations/20251029153500_games_db.sql
+++ backend/internal/supabase/migrations/20251029153500_games_db.sql
@@ -9,7 +9,8 @@ CREATE TYPE game_category AS ENUM (
     'speech'
 );
 
-CREATE TYPE game_type AS ENUM (
+-- QuestionType Enum
+CREATE TYPE question_type AS ENUM (
     'sequencing',
     'following_directions',
     'wh_questions',
@@ -28,30 +29,33 @@ CREATE TYPE game_type AS ENUM (
 -- Create GameContent Table
 CREATE TABLE game_content (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    category game_category NOT NULL,
-    game_type game_type NOT NULL,
-    difficulty_level INT NOT NULL CHECK ( difficulty_level >= 0 ),
+    theme_id UUID NOT NULL,
+    week INT NOT NULL CHECK ( week >= 0 AND week <= 6 ),
+    category game_category,
+    question_type question_type NOT NULL,
+    difficulty_level INT NOT NULL CHECK ( difficulty_level >= 1 ),
     question TEXT NOT NULL,
     options TEXT[] NOT NULL,
     answer TEXT NOT NULL,
     created_at TIMESTAMPTZ DEFAULT now(),
     updated_at TIMESTAMPTZ DEFAULT now()
+
+    FOREIGN KEY (theme_id) REFERENCES theme(theme_id) ON DELETE RESTRICT
 );
 
 -- Create GameResult Table
 CREATE TABLE game_result (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    session_id UUID NOT NULL,
-    student_id UUID NOT NULL,
+    session_student_id INT NOT NULL,
     content_id UUID NOT NULL,
-    time_taken INTEGER NOT NULL CHECK ( time_taken >= 0 ),
+    time_taken_sec INTEGER NOT NULL CHECK ( time_taken_sec >= 0 ),
     completed BOOLEAN NOT NULL DEFAULT FALSE,
     count_of_incorrect_attempts INTEGER NOT NULL DEFAULT 0 CHECK ( count_of_incorrect_attempts >= 0 ),
     incorrect_attempts TEXT[] DEFAULT '{}',
     created_at TIMESTAMPTZ DEFAULT now(),
     updated_at TIMESTAMPTZ DEFAULT now(),
 
-    FOREIGN KEY (session_id, student_id) REFERENCES session_student(session_id, student_id) ON DELETE CASCADE,
+    FOREIGN KEY (session_student_id) REFERENCES session_student(id) ON DELETE CASCADE,
     FOREIGN KEY (content_id) REFERENCES game_content(id) ON DELETE RESTRICT
 );
 
