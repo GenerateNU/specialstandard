d2db52ff3825a92c60ba772019dfc74e3be28ae8
kevinmatula <Matulakevin91@gmail.com>
2025-11-10 14:40:49 -0500

    crash out imminent

--- backend/internal/storage/postgres/schema/session_resource.go
+++ backend/internal/storage/postgres/schema/session_resource.go
@@ -39,10 +39,10 @@ func (sr *SessionResourceRepository) DeleteSessionResource(ctx context.Context, 
 func (sr *SessionResourceRepository) GetResourcesBySessionID(ctx context.Context, sessionID uuid.UUID, pagination utils.Pagination) ([]models.Resource, error) {
     // enforce default pagination cuz it keeps erroring
     if pagination.Limit == 0 {
-    	pagination.Limit = utils.NewPagination().Limit
+    	pagination.Limit = 10
 	}
 	if pagination.Page == 0 {
-    	pagination.Page = utils.NewPagination().Page
+    	pagination.Page = 1
 	}
     resources := make([]models.Resource, 0)
 
