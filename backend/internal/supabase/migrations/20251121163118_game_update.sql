05d69ebd55a548d58227771d73115b942b47079c
adescoteaux1 <ally.descoteaux.0@gmail.com>
2025-11-24 20:45:55 -0500

    update

--- backend/internal/supabase/migrations/20251121163118_game_update.sql
+++ backend/internal/supabase/migrations/20251121163118_game_update.sql
@@ -1,7 +1,8 @@
 CREATE TYPE exercise_type AS ENUM ('game', 'pdf');
+CREATE TYPE game_type AS ENUM ('drag and drop', 'spinner', 'word/image matching', 'flashcards');
 
 ALTER TABLE game_content
 ADD COLUMN exercise_type exercise_type NOT NULL DEFAULT 'game';
 
 ALTER TABLE game_content
-ADD COLUMN applicable_game_types question_type[] DEFAULT '{}';
+ADD COLUMN applicable_game_types game_type[] DEFAULT '{}';
